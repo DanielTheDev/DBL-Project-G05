73754566060bf8de8856ed04a42e021101dc9950
BuildTools <unconfigured@null.spigotmc.org>
2022-06-15 17:19:05 +0200

    changed sort delay

--- src/io/github/danielthedev/robot/util/Delay.java
+++ src/io/github/danielthedev/robot/util/Delay.java
@@ -20,8 +20,8 @@ public class Delay extends SequenceFunction {
 
 	public static final int CONVEYER_BELT_MOVE_DELAY = 1000;
 	public static final int CONVEYER_BELT_TIMEOUT = 1600;
-	public static final int BLACK_DISK_MOVE_DELAY = 2500;
-	public static final int WHITE_DISK_MOVE_DELAY = 2000;
+	public static final int BLACK_DISK_MOVE_DELAY = 3000;
+	public static final int WHITE_DISK_MOVE_DELAY = 2500;
 	public static final int DISK_PASS_DELAY = 9000;
 
 	public static final Object INTERCEPTOR = new Object();
